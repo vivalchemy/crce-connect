5651aa6cf0a5a58deef13e269df3b561633aeafe
Aryan <aryanfiger18@gmail.com>
2023-11-03 19:57:09 +0530

    {random message liv}

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,84 @@
+<script>
+  import NavbarSpacer from "$lib/components/block/NavbarSpacer.svelte";
+</script>
+
+<NavbarSpacer />
+<h4><strong>|ABOUT OUR JOURNEY IN TIME</strong></h4>
+
+<p>
+  Located in a picturesque environment in the heart of the Mumbai city,
+  Fr.Conceicao Rodrigues College of Engineering (Fr.CRCE) is one of the
+  renowned, premier and sought-after private Engineering colleges. The Institute
+  is dedicated to making a difference in Engineering education, with its
+  exclusive approach towards the ongoing momentum of trends in technology and
+  holistic development.
+</p>
+
+<p>
+  Beginning with an orphanage (BalBhavan) and a trade school in carpentry, the
+  Agnel Ashram (set up in 1957 at Bandra, Mumbai) has today grown into a
+  full-fledged Technical Complex. The Institute today proudly bears the
+  founder's name as a fitting tribute to his impassioned faith in
+  highly-qualified and fully-trained engineers and technicians in the service of
+  the nation.
+</p>
+
+<img
+  src="https://fragnel.edu.in/images/crce/about/college-with-clouds.jpg"
+  alt="College with Clouds"
+/>
+
+<p>
+  The Institute started with only one programme in Production Engineering in
+  1984 with an intake capacity of 60 students. In 1987, the programme in
+  Electronics Engineering was started with an intake capacity of 60 students,
+  followed by Computer Engineering in 1991 with an intake capacity of 60
+  students. In 2001, the programme in Information Technology was started with an
+  intake capacity of 30 students, which was later increased to 60 in 2010. The
+  College started offering Master's Degree in Electronics Engineering and
+  Mechanical Engineering in the year 2005, each having an intake capacity of 18.
+  Doctoral programs in Electronics Engineering and Mechanical Engineering
+  commenced in 2014 and 2015 respectively. From the year 2019, a new
+  Undergraduate degree programme in Mechanical Engineering has been started with
+  an intake capacity of 60. Also, the BE undergraduate programme in Electronics
+  Engineering has been changed to Electronics and Computer Science. The
+  Undergraduate programme in Information Technology has been merged with
+  Computer Engineering. From the year 2020, college is offering a new
+  undergraduate programme in the emerging area of 'AI & Data Science'. Also, a
+  Doctoral program in Computer Engineering started in the year 2023.
+</p>
+
+<p>
+  The Institute has been consistently ranked highly in major surveys like TOI,
+  INDIA TODAY, Career 360, The Week, and Education World to name a few. The
+  College is ranked by NIRF in 2017 and 2020. Based on the Outcome-Based
+  Education (OBE) parameters and criteria, three programmes of the institute
+  have been accredited by the National Board of Accreditation (NBA-India) since
+  2017-2018. The current accreditation is valid until June 2023. The college is
+  also NAAC accredited with grade 'A'.
+</p>
+
+<h4><strong>|AT A GLANCE</strong></h4>
+
+<p>The number of students graduates and undergraduates from our college</p>
+
+<p>GRADUATES<br />5000</p>
+
+<p>MAJORS<br />20</p>
+
+<p>CLASSROOMS<br />20</p>
+
+<p>STUDENTS<br />1624</p>
+
+<h4><strong>|TAKE A TOUR</strong></h4>
+
+<p>
+  <a href="https://www.youtube.com/watch?v=ZMDlPfqqAsY">TOUR OF Fr. CRCE</a>
+</p>
+
+<p>OUR CAMPUSES ARE LIVING LABORATORIES FOR SUSTAINABILITY.</p>
+
+<p>
+  The College is located at Bandstand, Bandra, Mumbai in a picturesque
+  environment surrounded by the Arabian Sea.
+</p>
